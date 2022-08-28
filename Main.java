2e075ed8097a861136da9b7cdf69f071b4ae68a5
Todd Teis <toddteis@gmail.com>
2022-08-29 09:50:33 +1000

    initial comment

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'K';
+        char studentLastInitial = 'H';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
     }
 }
