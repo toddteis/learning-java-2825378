6be916e1cbfd81aaa0398b63aabcb44939c6187d
Todd Teis <toddteis@gmail.com>
2022-08-31 06:01:30 +1000

    Initial commit.

--- Main.java
+++ Main.java
@@ -1,15 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int studentAge = 15;
-        double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
-        boolean hasPerfectAttendance = true;
 
-        System.out.println(studentAge);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
 
     }
 }
