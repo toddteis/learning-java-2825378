7ec85f2ae78c7724a8b9a6aacb6bd68e2ebe681e
Todd Teis <toddteis@gmail.com>
2022-08-31 06:31:58 +1000

    Added vacation days taken.

--- Main.java
+++ Main.java
@@ -2,22 +2,34 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static double calculateEmployeeSalary(double hoursWorkedPerWeek, double hourlyRate) {
+    public static double calculateEmployeeSalary(double hoursWorkedPerWeek, double hourlyRate, double totalVacationDays) {
         double result;
-        double totalHoursWorkedForYear = hoursWorkedPerWeek * 52;
+        double totalHoursWorkedForYear;
+        double totalVacationHoursTaken;
+
+        totalVacationHoursTaken = totalVacationDays * 8;
+        totalHoursWorkedForYear = (hoursWorkedPerWeek * 52) - totalVacationHoursTaken;
         result = totalHoursWorkedForYear * hourlyRate;
+
         return result;
     }
 
     public static void main(String[] args) {
         double hoursWorkedPerWeek;
         double hourlyRate;
+        double totalVacationDays;
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter hours worked per week.");
         hoursWorkedPerWeek = input.nextInt();
+
         System.out.println("Enter hourly rate.");
         hourlyRate = input.nextInt();
-        double grossYearlySalary = calculateEmployeeSalary(hoursWorkedPerWeek, hourlyRate);
+
+        System.out.println("Enter total vacation days taken.");
+        totalVacationDays = input.nextInt();
+
+        double grossYearlySalary = calculateEmployeeSalary(hoursWorkedPerWeek, hourlyRate, totalVacationDays);
         System.out.println("Your Gross Yearly Salary is " + grossYearlySalary);
     }
 }
