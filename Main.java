44553e482f57e3bc73cb8bb871cea6646337061d
Todd Teis <toddteis@gmail.com>
2022-08-31 06:17:24 +1000

    Working calculator.

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static double calculateEmployeeSalary(double hoursWorkedPerWeek, double hourlyRate) {
+        double result;
+        result = (hoursWorkedPerWeek * hourlyRate) * 52;
+        return result;
+    }
+
     public static void main(String[] args) {
-
-
+        double hoursWorkedPerWeek;
+        double hourlyRate;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter hours worked per week.");
+        hoursWorkedPerWeek = input.nextInt();
+        System.out.println("Enter hourly rate.");
+        hourlyRate = input.nextInt();
+        double grossYearlySalary = calculateEmployeeSalary(hoursWorkedPerWeek, hourlyRate);
+        System.out.println("Your Gross Yearly Salary is " + grossYearlySalary);
     }
 }
